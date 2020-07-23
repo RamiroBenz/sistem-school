2e425039a7846cc0e7b7bbcb3ec5e95ef641cec7
RamiroBenz <ramiro.benzacar@hotmail.com>
2020-07-23 09:09:12 -0300

    Add files via upload

---
+++ conexion/cn.php
@@ -0,0 +1,10 @@
+<?php 
+$conexion = mysqli_connect("localhost", "root", "root", "escuela");
+/*if(!$conexion){
+	echo 'Error al conectar la base de datos';
+}else{
+	echo '<b>Conectando la base de datos para empezar a operar..</b>
+	<br>';
+}*/
+
+ ?>
